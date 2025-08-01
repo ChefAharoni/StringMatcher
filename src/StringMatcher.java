aa5c29c28e0e6431a87011ad2da2d436397a0e1c
Amit Aharoni <ChefAharoni@proton.me>
2025-08-01 13:42:59 -0400

    * Changed class to a non-static * Moved some functionality to outer functions * Introduced instance variables

--- src/StringMatcher.java
+++ src/StringMatcher.java
@@ -1,22 +1,120 @@
 import java.io.*;
 import java.util.*;
-import java.util.stream.Collectors;
+//import java.util.stream.Collectors;
 
 public class StringMatcher
 {
-    String RESET = "\u001B[0m";
-    String GREEN = "\u001B[32m";
 
-    public static int horspool(String text, String pattern)
+    private String pattern;
+    private StringBuilder outputText;
+    private int occurrences;
+
+    private final String RESET = "\u001B[0m";
+    private final String GREEN = "\u001B[32m";
+
+
+    /**
+     * Constructor
+     * @param algorithm The string matching algorithm to perform,
+     *                  naive or Horspool.
+     * @param filePath The path to the input text file,
+     *                 including file name and extension.
+     * @param pattern The text pattern to look inside the text file.
+     */
+    public StringMatcher(String algorithm, String filePath, String pattern)
+    {
+        // algorithm
+        this.pattern = pattern;
+        this.outputText = new StringBuilder();
+
+        String text;
+        this.occurrences = -1;
+
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath)))
+        {
+            /* TODO - Read chunks of chars, not all file
+            You must read between 131,072 and 262,144 characters in a block of data,
+            process the block, and print the results to the screen.
+            Do not read the entire file into memory at once; some inputs may be too large.
+             */
+//            text = br.lines().collect(Collectors.joining(System.lineSeparator()));
+
+            text = br.readLine(); // TODO: change
+
+            // by here we've already checked that the algorithm var is valid
+           this.occurrences = algorithm.compareToIgnoreCase("horspool") == 0
+                    ? horspool(text, pattern) : naive(text, pattern);
+
+        } catch (IOException ioe)
+        {
+            System.err.println(ioe);
+            System.exit(1);
+        }
+    }
+
+    private String readFile(String filePath)
+    {
+        // TODO
+        return "";
+    }
+
+    public int horspool(String text, String pattern)
     {
         // TODO
         return -1;
     }
 
-    public static int naive(String text, String pattern)
+    public int naive(String text, String pattern)
     {
-        // TODO
+        int patternLen = pattern.length();
+        // call the core x times
+        int chunk = pattern.length(); // TODO - change
+        for (int i = 0; i < text.length() - patternLen; i++)
+        {
+            String subText = text.substring(i, i + (patternLen - 1));
+            int index = naiveCore(subText, pattern);
+            if (index != -1)
+            {
+                this.occurrences++;
+                markIndex(text, index);
+            }
+        }
+
+        return this.occurrences;
+    }
+
+    private void markIndex(String text, int index)
+    {
+        StringBuilder sb = new StringBuilder(text.length());
+    }
+
+    private int naiveCore(String text, String pattern)
+    {
+        int n = text.length(),
+                m = pattern.length();
+
+        for (int i = 0, end = n - m; i <= end; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                if (pattern.charAt(j) != text.charAt(i + j))
+                    break;
+
+                if (j == m-1) return i;
+            }
+        }
+
         return -1;
+    }
+
+    public void printSolution()
+    {
+        // TODO: implement timings
+        double timing = -1.0;
+
+        // TODO: change occurrences to singular if one
+        System.out.println("Occurrences of \"" + pattern + "\": " + occurrences);
+        System.out.println("Search time: " + timing + " ms");
     }
 
 
@@ -40,33 +138,11 @@ public class StringMatcher
             System.exit(1);
         }
 
-        String text;
-        int occurrences = -1;
+        StringMatcher sm = new StringMatcher(algorithm, filePath, pattern);
+        sm.printSolution();
 
-        try (BufferedReader br = new BufferedReader(new FileReader(filePath)))
-        {
-            /* TODO - Read chunks of chars, not all file
-            You must read between 131,072 and 262,144 characters in a block of data,
-            process the block, and print the results to the screen.
-            Do not read the entire file into memory at once; some inputs may be too large.
-             */
-            text = br.lines().collect(Collectors.joining(System.lineSeparator()));
 
-            // by here we've already checked that the algorithm var is valid
-            occurrences = algorithm.compareToIgnoreCase("horspool") == 0
-                    ? horspool(text, pattern) : naive(text, pattern);
-
-        } catch (IOException ioe)
-        {
-            System.err.println(ioe);
-            System.exit(1);
-        }
-
-        double timing = -1.0;
-
-        // TODO: change occurrences to singular if one
-        System.out.println("Occurrences of \"" + pattern + "\": " + occurrences);
-        System.out.println("Search time: " + timing + " ms");
         System.exit(0);
+
     }
 }
